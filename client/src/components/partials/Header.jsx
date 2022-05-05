45891e4aee66ba0e2141b285fcb46132dd7dce55
stesevicuros <stesevicu@gmail.com>
2022-05-05 21:12:04 +0200

    Finished ExplorePage

--- client/src/components/partials/Header.jsx
+++ client/src/components/partials/Header.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 
 export default function Header() {
-	return (
-		<>
-			<div className='header'>
-				<h4 className='logo'>BlogMe</h4>
-				<div className='buttons'>
-					<div className='explore'>
-						<a>Explore</a>
-					</div>
-					<div className='explore'>
-						<a>Login</a>
-					</div>
-					<div className='explore'>
-						<a>Register</a>
-					</div>
-				</div>
-			</div>
-			<div className='border-bottom'></div>
-		</>
-	);
+    return (
+        <>
+            <div className='header'>
+                <h4 className='logo'>BlogMe</h4>
+                <div className='buttons'>
+                    <div className='explore'>
+                        <a>Explore</a>
+                    </div>
+                    <div className='explore'>
+                        <a>Login</a>
+                    </div>
+                    <div className='explore'>
+                        <a>Register</a>
+                    </div>
+                </div>
+            </div>
+            <div></div>
+            <div className='border-bottom'></div>
+        </>
+    );
 }

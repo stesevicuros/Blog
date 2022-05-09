1c3079799eb89957355405576edaa8bb8f614ccc
stesevicuros <stesevicu@gmail.com>
2022-05-09 18:00:59 +0200

    Created OpenPostPage

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import ExplorePage from './components/pages/ExplorePage';
 import LoginPage from './components/pages/LoginPage';
 import RegisterPage from './components/pages/RegisterPage';
 import CreatePostPage from './components/pages/CreatePostPage';
+import OpenPostPage from './components/pages/OpenPostPage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
                 <Route path='/login' element={<LoginPage />} />
                 <Route path='/register' element={<RegisterPage />} />
                 <Route path='/new-post' element={<CreatePostPage />} />
+                <Route path='/post:id' element={<OpenPostPage />} />
             </Routes>
         </Router>
     );
---
+++ client/src/components/pages/OpenPostPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../partials/Header';
+
+export default function OpenPostPage() {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+}

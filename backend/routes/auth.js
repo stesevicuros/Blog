68429ca74e45ff9f4ca2b361a9b45d258cfd4ebf
stesevicuros <stesevicu@gmail.com>
2022-05-15 19:15:00 +0200

    Finished login and register

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,52 +5,80 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user.model');
 
 router.post('/register', (req, res) => {
-	User.findOne({ email: req.body.email }).then(user => {
-		if (user) {
-			return res
-				.status(400)
-				.json({ email: 'Email already exists' });
-		} else {
-			let newUser = new User({
-				email: req.body.email,
-				password: req.body.password
-			});
-			bcrypt
-				.hash(newUser.password, 10)
-				.then((err, hash) => {
-					if (err) {
-						console.log(err);
-					}
-					newUser.password = hash;
-					newUser
-						.save()
-						.then(user => res.json(user))
-						.catch(err => console.log(err));
+	User.findOne({ username: req.body.username }).then(
+		user => {
+			if (user) {
+				return res.status(400).json({
+					username: 'Username already exists'
 				});
+			} else {
+				const username = req.body.username;
+
+				bcrypt.hash(
+					req.body.password,
+					10,
+					(err, hash) => {
+						const newUser = new User({
+							username: username,
+							password: hash
+						});
+
+						newUser
+							.save()
+							.then(() => {
+								req.session.authenticated = true;
+								req.session.userID =
+									newUser._id;
+								req.session.username =
+									newUser.username;
+
+								res.status(200).json(
+									'Account created. You are now logged in!'
+								);
+							})
+							.catch(err => res.send(err));
+					}
+				);
+			}
 		}
-	});
+	);
 });
 
 router.post('/login', (req, res) => {
-	User.findOne({ email: req.body.email }).then(user => {
-		if (user) {
-			bcrypt
-				.compare(req.body.password, user.password)
-				.then(isCorrectPassword => {
-					if (isCorrectPassword) {
-						res.json('Logged in');
-					} else {
-						res.status(403).json(
-							'Wrong password'
-						);
+	User.findOne({ username: req.body.username }).then(
+		user => {
+			if (user) {
+				bcrypt.compare(
+					req.body.password,
+					user.password,
+					(err, result) => {
+						if (result) {
+							req.session.authenticated = true;
+							req.session.userID = user._id;
+							req.session.username =
+								user.username;
+							res.status(200).json(
+								'You are now logged in!'
+							);
+						} else {
+							res.status(401).json(
+								'Incorrect password.'
+							);
+						}
 					}
-				});
-		} else {
-			res.status(404).json({
-				email: "Email doesn't exists"
-			});
+				);
+			} else {
+				res.status(404).json('Incorrect username.');
+			}
 		}
-	});
+	);
+});
+
+router.post('/logout').post((req, res) => {
+	req.session.authenticated = false;
+	req.session.userID = undefined;
+	req.session.username = undefined;
+	res.send('Goodbye!');
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,31 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
+const session = require('express-session');
 const cors = require('cors');
 
 const app = express();
 
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		cookie: { maxAge: 10000 * 60 * 30 },
+		saveUninitialized: false,
+		resave: false
+	})
+);
+app.use(
+	cors({
+		origin: 'http://localhost:3000'
+	})
+);
+
 app.use(express.json());
 
 const AuthRoute = require('./routes/auth');
-const routes = require('./routes.js');
 
-mongoose.connect(process.env.MONGOOSE_CONNECTION);
+const uri = process.env.MONGOOSE_CONNECTION;
+mongoose.connect(uri);
 mongoose.connection.once('open', () =>
 	console.log('Connected to MongoDB')
 );
@@ -18,11 +33,6 @@ mongoose.connection.on('error', () =>
 	console.log('Could not connect to database')
 );
 
-app.use(
-	cors({
-		origin: 'http://localhost:3000'
-	})
-);
 app.use('/api', AuthRoute);
 
 app.listen(3001, () => {
